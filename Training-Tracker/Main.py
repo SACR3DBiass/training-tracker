4f68eb9dafdc1dc30a8dd686f72b6403b4ca750d
connor-dev <connorvallance@gmail.com>
2026-02-06 11:00:51 +0200

    Add files via upload

---
+++ Training-Tracker/Main.py
@@ -0,0 +1,17 @@
+bash
+
+gti init 
+git status
+bash
+
+git remote add origin https://github.com/SACR3DBiass/training-tracker.git
+
+python
+print("Training tracker started")
+
+bash
+
+git add .
+git commit -m "Initial project setup"
+git push -u origin main
+
